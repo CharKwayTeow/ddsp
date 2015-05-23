d623b9904e4fa1967d8f83b45d39c9b57d2a4b0e
CharKwayTeow <wug.lim@gmail.com>
2015-05-22 23:09:04 -0700

    Add a version field into the header.

--- DDSP/Header.py
+++ DDSP/Header.py
@@ -7,18 +7,20 @@ from MessageType import MessageType
 class Header:
     """docstring for Header"""
     def __init__(self, type = MessageType.undefined):
+        self.version = 1
         self.type = type
         self.length = 0
         self.port = 0
 
     def encapsulate(self):
-        return struct.pack("!BHH", self.type, self.length, self.port)
+        return struct.pack("!BBHH", self.version, self.type, self.length, self.port)
 
     def decapsulate(self, data):
-        header = struct.unpack("!BHH", data)
-        self.type = header[0]
-        self.length = header[1]
-        self.port = header[2]
+        header = struct.unpack("!BBHH", data)
+        self.version = header[0]
+        self.type = header[1]
+        self.length = header[2]
+        self.port = header[3]
 
 """Write the test code here"""
 if __name__ == '__main__':
@@ -26,6 +28,7 @@ if __name__ == '__main__':
     print str(header.port)
     header.length = 1
     header.decapsulate(header.encapsulate())
+    print header.version
     print header.type
     print header.length
     print header.port
