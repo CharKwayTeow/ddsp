5a1bffdad9efb930d26df9f1d47df8a58db15f55
CharKwayTeow <wug.lim@gmail.com>
2015-05-26 16:08:53 -0700

    Updated the test case.

--- tests/Subscriber.py
+++ tests/Subscriber.py
@@ -5,7 +5,7 @@ sys.path.append('../DDSP/')
 from DDSP import DDSP
 
 if __name__ == '__main__':
-    ddsp = DDSP('../../sent', 12344)
+    ddsp = DDSP('../../sent')
     time.sleep(3)
     ddsp.requestContent('123456789012345678901234567890123456789012345678901234567890abcd')
     time.sleep(10)
