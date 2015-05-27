87ce7cd0fcf660dd903f53876c47343576284bea
CharKwayTeow <wug.lim@gmail.com>
2015-05-26 22:47:41 -0700

    Fixed bugs in sending and receiving data.

--- tests/Publisher.py
+++ tests/Publisher.py
@@ -6,6 +6,7 @@ from DDSP import DDSP
 
 if __name__ == '__main__':
     ddsp = DDSP('../../sent')
+    time.sleep(3)
     ddsp.addContent(b'123456789012345678901234567890123456789012345678901234567890abcd')
     time.sleep(10)
     print ("Reached the end of Publisher.py")
