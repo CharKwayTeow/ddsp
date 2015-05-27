7baf72eb109ca46b492f0b5f17619dd4a57218ad
CharKwayTeow <wug.lim@gmail.com>
2015-05-26 23:41:58 -0700

    Fixed a bug in test case.

--- tests/Publisher.py
+++ tests/Publisher.py
@@ -5,7 +5,7 @@ sys.path.append('../DDSP/')
 from DDSP import DDSP
 
 if __name__ == '__main__':
-    ddsp = DDSP('../../sent')
+    ddsp = DDSP('vboxnet0', '../../sent')
     time.sleep(3)
     ddsp.addContent(b'123456789012345678901234567890123456789012345678901234567890abcd')
     time.sleep(10)
