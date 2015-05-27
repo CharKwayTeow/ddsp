5dc40813c492386683e471c92b7f5ba912c93d5a
CharKwayTeow <wug.lim@gmail.com>
2015-05-26 17:43:13 -0700

    Fixed bugs in listening to the port.

--- tests/Subscriber.py
+++ tests/Subscriber.py
@@ -7,6 +7,6 @@ from DDSP import DDSP
 if __name__ == '__main__':
     ddsp = DDSP('../../sent')
     time.sleep(3)
-    ddsp.requestContent('123456789012345678901234567890123456789012345678901234567890abcd')
+    ddsp.requestContent(b'123456789012345678901234567890123456789012345678901234567890abcd')
     time.sleep(10)
     print ("Reached the end of Subscriber.py")
