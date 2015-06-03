46246ea91eac01e66d913b561c518e4e26b04532
CharKwayTeow <wug.lim@gmail.com>
2015-06-02 22:18:58 -0700

    Added multithreads features in Monitor.

--- demos/demo_1/Monitor.py
+++ demos/demo_1/Monitor.py
@@ -4,23 +4,29 @@ import os
 import sys
 import random
 import hashlib
+import _thread
 sys.path.append('../../DDSP/')
 from DDSP import DDSP
+
+def updateScreen(stdscr):
+    while True:
+        stdscr.clear()
+        stdscr.addstr(0, 10, "A Monitor Using the APIs of DDSP")
+        stdscr.addstr(1, 0, "Press 'q' to quit.")
+        stdscr.addstr(3, 0, "FID \t IP Address \t TTL \t Status \n")
+        i = 4
+        for record_dict in ddsp.getResourceTable():
+            stdscr.addstr(i, 0, record_dict['fid'] + "\t" + record_dict['ip_addr'] + "\t" + record_dict['ttl'] + "\t" + record_dict['status'] + "\n")
+            i += 1
+        stdscr.refresh()
+
 
 if __name__ == '__main__':
     ddsp = DDSP(sys.argv[1])
     stdscr = curses.initscr()
+    _thread.start_new_thread(updateScreen, (stdscr, ))
 
-    i = 4
     while True:
-        stdscr.addstr(0, 10, "A Monitor Using the APIs of DDSP")
-        stdscr.addstr(1, 0, "Press 'q' to quit.")
-        stdscr.addstr(3, 0, "FID \t IP Address \t TTL \t Status")
-        for record_dict in ddsp.getResourceTable():
-            stdscr.addstr(i, 0, record_dict['fid'] + "\t" + record_dict['ip_addr'] + "\t" + record_dict['ttl'] + "\t" + record_dict['status'])
-            i += 1
-        stdscr.refresh()
-
         key = stdscr.getch() 
         if key == ord('q'):
             curses.endwin()
