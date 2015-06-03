c24ce98c80597ef87b470026580a4b5e3a39c624
CharKwayTeow <wug.lim@gmail.com>
2015-06-02 17:05:46 -0700

    Fixed a bug in Monitor.py

--- demos/demo_1/Monitor.py
+++ demos/demo_1/Monitor.py
@@ -8,12 +8,11 @@ sys.path.append('../../DDSP/')
 from DDSP import DDSP
 
 if __name__ == '__main__':
-    ddsp = DDSP(sys.argv[1], sys.argv[2])
+    ddsp = DDSP(sys.argv[1])
     stdscr = curses.initscr()
 
     i = 4
     while True:
-        stdscr.clear()
         stdscr.addstr(0, 10, "A Monitor Using the APIs of DDSP")
         stdscr.addstr(1, 0, "Press 'q' to quit.")
         stdscr.addstr(3, 0, "FID \t IP Address \t TTL \t Status")
@@ -21,7 +20,7 @@ if __name__ == '__main__':
             stdscr.addstr(i, 0, record_dict['fid'] + "\t" + record_dict['ip_addr'] + "\t" + record_dict['ttl'] + "\t" + record_dict['status'])
             i += 1
         stdscr.refresh()
-        
+
         key = stdscr.getch() 
         if key == ord('q'):
             curses.endwin()
