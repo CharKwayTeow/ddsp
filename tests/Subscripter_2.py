923f8e04bbc8b8b91f227ff5a9b9677203401c7d
CharKwayTeow <wug.lim@gmail.com>
2015-06-02 23:23:48 -0700

    Fixed a but in test cases.

--- tests/Subscripter_2.py
+++ tests/Subscripter_2.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     ddsp = DDSP(sys.argv[1], sys.argv[2])
     while True:
         for record_dict in ddsp.getResourceTable():
+            if record_dict['ip_addr'] != '127.0.0.1':
                 ddsp.requestContent(record_dict['fid'].encode('utf-8'))
                 print (record_dict['fid'] + "\t" + record_dict['ip_addr'] + "\t" + record_dict['ttl'] + "\t" + record_dict['status'])
     os._exit(1)
