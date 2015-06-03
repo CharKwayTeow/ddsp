9541d910b6a5ed38825ffbea3754010a3bf642cc
CharKwayTeow <wug.lim@gmail.com>
2015-06-03 00:08:04 -0700

    Fixed a bug in the test case.

--- tests/Subscripter_2.py
+++ tests/Subscripter_2.py
@@ -9,6 +9,13 @@ if __name__ == '__main__':
     while True:
         for record_dict in ddsp.getResourceTable():
             if record_dict['ip_addr'] != '127.0.0.1':
-                ddsp.requestContent(record_dict['fid'].encode('utf-8'))
-                print (record_dict['fid'] + "\t" + record_dict['ip_addr'] + "\t" + record_dict['ttl'] + "\t" + record_dict['status'])
+                interested = True
+                for duplicated in ddsp.getResourceTable():
+                    if duplicated['fid'] == record_dict['fid'] and duplicated['ip_addr'] == '127.0.0.1':
+                        interested = False
+                        break
+                if interested:
+                    ddsp.requestContent(record_dict['fid'].encode('utf-8'))
+                    print (record_dict['fid'] + "\t" + record_dict['ip_addr'] + "\t" + record_dict['ttl'] + "\t" + record_dict['status'])
+        
     os._exit(1)
